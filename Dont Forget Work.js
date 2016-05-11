bf1ae177458c158de448d195542de61a9f51799b
calebanth <caleb.m.anthony@gmail.com>
2016-05-11 10:03:05 -0600

    V1.3.3
    Fixed the template literals.

--- Dont Forget Work.js
+++ Dont Forget Work.js
@@ -5,9 +5,9 @@
     let WL = document.getElementById('prgActionOverlay').innerHTML
     WL = (WL.split(' ')).pop()
     const currentWorkload = WL.split('/')[0]
-    document.title = '&{currentWorkload} actions left'
+    document.title = `&{currentWorkload} actions left`
     if (currentWorkload.length < 1 && isWorking === true) {
-      alert('Don\'t forget work, ${name} !')
+      alert(`Don\'t forget work, ${name} !`)
       isWorking = false
     }
     if (currentWorkload.length > 0 && isWorking === false) {
